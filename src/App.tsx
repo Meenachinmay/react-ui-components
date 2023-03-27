d3c9491b6f33a587c12a5f0d2fa25311a354db14
swt-realcnt <swt@real-cnt.com>
2023-03-28 01:35:24 +0530

    added styled-components and made a paypal similar navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import styled from "styled-components";
+import { FaBell } from "react-icons/fa";
+import { AiFillSetting } from "react-icons/ai";
+import { RiLogoutCircleRFill } from "react-icons/ri";
+
+const Container = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap");
+  font-family: Montserrat, "sans-serif";
+`;
+
+const Navbar = styled.div`
+  width: 100%;
+  height: 100px;
+  background-color: #152b8d;
+  color: #d1f1ff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  font-weight: bolder;
+`;
+
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const ListItem = styled.li`
+  margin-left: 5px;
+  margin-right: 5px;
+  padding: 10px 16px;
+  text-align: center;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 20px;
+  transition: ease-in 0.4s;
+
+  &:hover {
+    background-color: #283d9bbe;
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  cursor: pointer;
+`;
+
+const RightList = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  gap: 20px;
+`;
+
+const RightListItem = styled.li`
+  font-size: 20px;
+`;
+
+const Logo = styled.img`
+  height: 60px;
+  object-fit: cover;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Container>
+        <Navbar>
+          <Left>
+            <List>
+              <Logo src="./img/paypal.png" />
+              <ListItem>Dashboard</ListItem>
+              <ListItem>Send and Request</ListItem>
+              <ListItem>Payment Methods</ListItem>
+              <ListItem>Activity</ListItem>
+              <ListItem>Help</ListItem>
+            </List>
+          </Left>
+          <Right>
+            <RightList>
+              <RightListItem>
+                <FaBell />
+              </RightListItem>
+              <RightListItem>
+                <AiFillSetting />
+              </RightListItem>
+              <RightListItem>
+                <RiLogoutCircleRFill />
+              </RightListItem>
+            </RightList>
+          </Right>
+        </Navbar>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/icons/Bell.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Bell: React.FC = () => {
+  return (
+    <>
+      <svg
+        viewBox="-102.4 -102.4 1228.80 1228.80"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#ffffff"
+        stroke="#ffffff"
+      >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          <path
+            fill="#000000"
+            d="M512 64a64 64 0 0 1 64 64v64H448v-64a64 64 0 0 1 64-64z"
+          ></path>
+          <path
+            fill="#000000"
+            d="M256 768h512V448a256 256 0 1 0-512 0v320zm256-640a320 320 0 0 1 320 320v384H192V448a320 320 0 0 1 320-320z"
+          ></path>
+          <path
+            fill="#000000"
+            d="M96 768h832q32 0 32 32t-32 32H96q-32 0-32-32t32-32zm352 128h128a64 64 0 0 1-128 0z"
+          ></path>
+        </g>
+      </svg>
+    </>
+  );
+};
+
+export default Bell;
